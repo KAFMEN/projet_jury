30cad292cf39f9cf2d3ca07a12902bc35253b0ce
kafmen <kafmen@hotmail.fr>
2022-02-24 14:51:20 +0100

    soleil celia

--- src/Controller/Customer/HomeController.php
+++ src/Controller/Customer/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Customer;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,11 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('customer/index.html.twig');
     }
 }
